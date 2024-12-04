1789be70906d096285af1d30485ff6ce7ca6774d
thebadams <bpeteradams@gmail.com>
2024-12-03 22:22:36 -0500

    Day2(part1): wrote a function to analyze a slice of ints passed to it for different fail scenarios

---
+++ day-2/AnalyzeData.go
@@ -0,0 +1,32 @@
+package day2
+
+func AnalyzeData(data []int) bool {
+
+	increases := 0
+	decreases := 0
+	// count numbers of increase, decrease, no change between numbers
+	for i, v := range data {
+		if v > data[i+1] {
+			// if difference is greater than 3, unsafe data return false
+			if v-data[i+1] > 3 {
+				return false
+			}
+			decreases = decreases + 1
+		} else if v < data[i+1] {
+			// if difference is greater than 3, unsafe data return false
+			if data[i+1]-v > 3 {
+				return false
+			}
+			increases = increases + 1
+		} else {
+			// if there was neither an increase nor decrease, immediately return false. Unsafe data
+			return false
+		}
+	}
+	// return false if there were both increases and decreases
+	if increases > 0 && decreases > 0 {
+		return false
+	}
+
+	return true
+}
---
+++ day-2/AnalyzeData_test.go
@@ -0,0 +1,8 @@
+package day2
+
+import "testing"
+
+func TestAnalyzeData(t *testing.T) {
+	//TODO: Write tests for the different scenarios
+
+}
