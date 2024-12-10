dc31a4890249da0e991ff6dbde31b9c5b9a801dc
thebadams <bpeteradams@gmail.com>
2024-12-09 21:38:14 -0500

    Day2(refactor): refactored business logic in Analyze Data function

--- day-2/AnalyzeData.go
+++ day-2/AnalyzeData.go
@@ -1,35 +1,28 @@
 package day2
 
 func AnalyzeData(data []int) bool {
-
-	increases := 0
 	decreases := 0
-	// count numbers of increase, decrease, no change between numbers
-	for i, v := range data {
-		if i < len(data)-1 {
-			if v > data[i+1] {
-				// if difference is greater than 3, unsafe data return false
-				if v-data[i+1] > 3 {
-					return false
-				}
-				decreases = decreases + 1
-			} else if v < data[i+1] {
-				// if difference is greater than 3, unsafe data return false
-				if data[i+1]-v > 3 {
-					return false
-				}
-				increases = increases + 1
-			} else {
-				// if there was neither an increase nor decrease, immediately return false. Unsafe data
-				return false
-			}
+	increases := 0
+	for i := 1; i < len(data); i++ {
+		// compare to previous value
+		diff := data[i] - data[i-1]
 
+		if diff > 0 {
+			increases = increases + 1
+		}
+		if diff < 0 {
+			decreases = decreases + 1
+		}
+		if increases > 0 && decreases > 0 {
+			return false
+		}
+		if diff > 3 || diff < -3 {
+
+			return false
+		}
+		if diff == 0 {
+			return false
 		}
 	}
-	// return false if there were both increases and decreases
-	if increases > 0 && decreases > 0 {
-		return false
-	}
-
 	return true
 }
